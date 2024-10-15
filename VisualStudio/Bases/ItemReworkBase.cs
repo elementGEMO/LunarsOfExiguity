06f5131ab8650f3e70cb952d383709b444a96510
Remurin <73508129+ItsRemurin@users.noreply.github.com>
2024-10-14 23:10:17 -0400

    Fix ItemReworkBase for Name Configs

--- VisualStudio/Bases/ItemReworkBase.cs
+++ VisualStudio/Bases/ItemReworkBase.cs
@@ -4,10 +4,10 @@ namespace LunarsOfExiguity;
 
 public abstract class ItemReworkBase : GenericBase<ItemDef>
 {
-    protected virtual string DisplayNameOverride { get; }
+    protected virtual string RelicNameOverride { get; }
+    protected virtual string CursedNameOverride { get; }
+    
     protected virtual string PickupOverride { get; }
     protected virtual string DescriptionOverride { get; }
     protected virtual string LoreOverride { get; }
-
-    protected virtual bool IsNameChangeEnabled() => true;
 }
