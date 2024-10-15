2968d34bea64c5dea70b80e20f769c6b0dea518d
Remurin <73508129+ItsRemurin@users.noreply.github.com>
2024-10-14 22:47:40 -0400

    BaseItem Rewrite

---
+++ VisualStudio/Bases/BaseItem.cs
@@ -0,0 +1,48 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace LunarsOfExiguity;
+
+public abstract class BaseItem
+{
+    protected abstract string Name { get; }
+    
+    protected virtual bool IsRemovable => false;
+    protected virtual bool IsHidden => false;
+    
+    protected virtual ItemTag[] Tags => [];
+    protected virtual CombinedItemTier Tier => ItemTier.NoTier;
+    
+    protected virtual string DisplayName { get; }
+    protected abstract string PickupText { get; }
+    protected abstract string Description { get; }
+    protected abstract string Lore { get; }
+    
+    public BaseItem() => Create();
+
+    private void Create()
+    {
+        _itemDef = ScriptableObject.CreateInstance<ItemDef>();
+        _itemDef.name = Name;
+
+        _itemDef.canRemove = IsRemovable;
+        _itemDef.hidden = IsHidden;
+
+        _itemDef.tags = Tags;
+        _itemDef._itemTierDef = Tier;
+        _itemDef.deprecatedTier = Tier;
+        
+        if (_itemDef)
+        {
+            _itemDef.AutoPopulateTokens();
+            _itemDef.nameToken = AssetStatics.tokenPrefix + _itemDef.nameToken;
+            _itemDef.pickupToken = AssetStatics.tokenPrefix + _itemDef.pickupToken;
+            _itemDef.descriptionToken = AssetStatics.tokenPrefix + _itemDef.descriptionToken;
+            _itemDef.loreToken = AssetStatics.tokenPrefix + _itemDef.loreToken;
+        }
+    }
+
+    public ItemDef Get() => _itemDef;
+
+    private ItemDef _itemDef;
+}
---
+++ VisualStudio/Bases/BaseItemTier.cs
@@ -0,0 +1,6 @@
+﻿namespace LunarsOfExiguity;
+
+public class BaseItemTier
+{
+    
+}
